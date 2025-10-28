54eca3ad443555ff574d4e5887fee1d179f19f9d
x灰 <celia.xu@etu.u-paris.fr>
2025-10-28 14:15:46 +0100

    Body homepage finalisé (#3)
    * Refactorisation dans la page d'accueil
    
    * Page d'accueil: avancement 2%...
    
    * Avancement 3%...
    
    * Avancement 4%...
    
    * Avancement 5%..
    
    * Avancement 9%...
    
    * Avancement 20%...
    
    * Avancement 40%...
    
    * Petit modification
    
    * Avancement 80%...
    
    * Avancement 90%...
    
    * Ajout des commentaires dans le HTML
    
    * Réorganisation du code CSS

---
+++ energiejeunes/assets/js/scroll.js
@@ -0,0 +1,30 @@
+let ul = document.querySelector('.item-slide ul');
+let lis = document.querySelectorAll('.item-slide li');
+let btns = document.querySelectorAll('.btn-soutien .btn');
+
+// Duplique le contenu
+ul.innerHTML = ul.innerHTML + ul.innerHTML;
+lis = document.querySelectorAll('.item-slide li');
+
+let speed = -1.5;
+let currentPosition = 0;
+
+function move() {
+    currentPosition += speed;
+
+    const singleSetWidth = ul.scrollWidth / 2;
+    if (currentPosition <= -singleSetWidth) {
+        currentPosition = 0;
+    }
+    if (currentPosition > 0) {
+        currentPosition = -singleSetWidth;
+    }
+    ul.style.left = currentPosition + 'px';
+}
+
+let timer = setInterval(move,40);
+
+ul.addEventListener('mouseenter',()=> clearInterval(timer));
+ul.addEventListener('mouseleave',()=> timer = setInterval(move, 16));
+btns[1].addEventListener('click',()=> speed = -1.5);
+btns[0].addEventListener('click',() => speed = 1.5);
