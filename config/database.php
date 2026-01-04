4bf0c9af31dddfadc7d49b03e576a3a125e146c1
FitiaRak <ny-aro-fitia.rakotosalama@etu.u-paris.fr>
2026-01-04 07:56:54 +0100

    test database

--- config/database.php
+++ config/database.php
@@ -1,9 +1,19 @@
 <?php
-$pdo = new PDO(
-    "mysql:host=localhost;dbname=benevoles;charset=utf8",
-    "root",
-    "password",
-    [
-        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
-    ]
-);
+//$pdo = new PDO(
+//    "mysql:host=localhost;dbname=benevoles;charset=utf8",
+//    "root",
+//    "password",
+//    [
+//        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+//    ]
+//);
+    $db_server = "127.0.0.1";
+    $db_username = "root";
+    $db_password = "";
+    $db_name = "benevolesdb";
+    $conn = "";
+
+    $conn = mysqli_connect($db_server,
+                            $db_username,
+                            $db_password,
+                            $db_name);
