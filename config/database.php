c11ef28aa4b4dd79d3b3266423b44d4019ceddd9
rakotosalamn <ny-aro-fitia.rakotosalama@etu.u-paris.fr>
2026-01-06 16:25:44 +0100

    ajout de la base de données sqlite et tableau bénévoles

--- config/database.php
+++ config/database.php
@@ -7,13 +7,17 @@
 //        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
 //    ]
 //);
-    $db_server = "127.0.0.1";
-    $db_username = "root";
-    $db_password = "";
-    $db_name = "benevolesdb";
-    $conn = "";
+//    $db_server = "127.0.0.1";
+//    $db_username = "root";
+//    $db_password = "";
+//    $db_name = "benevolesdb";
+//    $conn = "";
+//
+//    $conn = mysqli_connect($db_server,
+//                            $db_username,
+//                            $db_password,
+//                            $db_name);
 
-    $conn = mysqli_connect($db_server,
-                            $db_username,
-                            $db_password,
-                            $db_name);
+//pour se connecter:
+$pdo = new PDO("sqlite:" . __DIR__ . "/database.sqlite");
+
