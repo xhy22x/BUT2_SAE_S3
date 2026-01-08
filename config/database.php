9393b23ca45fb4c394eb4033f1fd6f8274e604e4
celia xu <celia.xu@etu.u-paris.fr>
2026-01-08 13:54:07 +0100

    rectification erreur

--- config/database.php
+++ config/database.php
@@ -1,11 +1,3 @@
 <?php
 
-try {
-    $pdo = new PDO(
-        'sqlite:' . __DIR__ . '../../database.sqlite'
-    );
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    die('Connection failed: ' . $e->getMessage());
-
-}
+$pdo = new PDO("sqlite:" . __DIR__ . "/database.sqlite");
